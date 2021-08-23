c932809424b81ef169615780d668c451efc954e7
Tomek <61616164+masa87@users.noreply.github.com>
2021-08-23 22:12:13 +0200

    task 08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const qs = (selector) => document.querySelector(selector);
 
+const form = qs(".login-form");
+const inputEmail = qs('input[name="email"]');
+const inputPass = qs('input[name="password"]');
+const btn = qs("button");
+
+form.addEventListener("submit", formValidation);
+
+function formValidation(e) {
+  e.preventDefault();
+  const { email, password } = e.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    alert("Wypełnij wszystkie wymagane pola!");
+  } else {
+    console.log(`Email: ${email.value}, Password: ${password.value}`);
+    e.currentTarget.reset();
+  }
+}
