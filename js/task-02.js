6d12d0f71484a8be5ca0706f4356f0a46560e8a5
Tomek <61616164+masa87@users.noreply.github.com>
2021-08-23 20:18:10 +0200

    task 02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const qs = (selector) => document.querySelector(selector);
+
+const ulIngredients = qs("#ingredients");
+
+for (let item of ingredients) {
+  const newIngredient = document.createElement("li");
+  newIngredient.textContent = item;
+  newIngredient.classList.add("item");
+  ulIngredients.append(newIngredient);
+}
