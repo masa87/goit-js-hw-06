26a11780e1e121d6b0edc10bd5794306356c71f7
Tomek <61616164+masa87@users.noreply.github.com>
2021-08-23 22:21:49 +0200

    task 09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
+const qs = (selector) => document.querySelector(selector);
+
+const body = qs("body");
+const spanColor = qs(".color");
+const changeColor = qs(".change-color");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function changeFunction() {
+  let newColor = getRandomHexColor();
+  spanColor.textContent = newColor;
+  body.style.backgroundColor = newColor;
+}
+
+changeColor.addEventListener("click", changeFunction);
