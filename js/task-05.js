a947fe1e9ab11a6e025b969561def2686e63802c
Tomek <61616164+masa87@users.noreply.github.com>
2021-08-23 21:11:21 +0200

    task 05

---
+++ js/task-05.js
@@ -0,0 +1,10 @@
+const qs = (selector) => document.querySelector(selector);
+
+const inputName = qs("#name-input");
+const outputName = qs("#name-output");
+
+const inputText = () => {
+  outputName.innerText = inputName.value;
+};
+
+inputName.addEventListener("change", inputText);
